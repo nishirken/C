0d1885beecbc1c1f6f508166c70d24df66fa73bf
Dmitrii Skurikhin <dskurikhin@devvela.com>
2017-09-16 17:07:32 +0500

    invert convertTemprature cycle

--- src/convertTemprature.c
+++ src/convertTemprature.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 void convertCelcius(int upper, int step) {
-    int celcius = 0;
+    int celcius = upper;
     printf("Fahrenheit\tCelcius\n");
-    for (celcius; celcius <= upper; celcius = celcius + step) {
+    for (celcius; celcius >= 0; celcius = celcius - step) {
         printf("%10.1f\t%7d\n", celcius, celcius * (9.0 / 5.0) + 32.0);
     }
 }
 
 void convertFahrenheit(int upper, int step) {
-    int fahr = 0;
+    int fahr = upper;
     printf("Celcius\tFahrenheit\n");
-    for (fahr; fahr <= upper; fahr = fahr + step) {
+    for (fahr; fahr >= 0; fahr = fahr - step) {
         printf("%7d\t%10.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
     }
 }
