3bc53f47182898f5d135e76ec355a454d7362c26
Dmitrii Skurikhin <dskurikhin@devvela.com>
2017-09-16 17:12:43 +0500

    added constants for convertTemprature

--- src/convertTemprature.c
+++ src/convertTemprature.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
-void convertCelcius(int upper, int step) {
-    int celcius = upper;
-    printf("Fahrenheit\tCelcius\n");
-    for (celcius; celcius >= 0; celcius = celcius - step) {
-        printf("%10.1f\t%7d\n", celcius, celcius * (9.0 / 5.0) + 32.0);
+void convertCelsius(int upper, int step) {
+    int celsius = upper;
+    printf("Fahrenheit\tCelsius\n");
+    for (celsius; celsius >= 0; celsius = celsius - step) {
+        printf("%10.1f\t%7d\n", celsius, celsius * (9.0 / 5.0) + 32.0);
     }
 }
 
 void convertFahrenheit(int upper, int step) {
     int fahr = upper;
-    printf("Celcius\tFahrenheit\n");
+    printf("Celsius\tFahrenheit\n");
     for (fahr; fahr >= 0; fahr = fahr - step) {
         printf("%7d\t%10.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
     }
 }
 
+#define CELSIUS_UPPER 100
+#define CELSIUS_STEP 5
+#define FAHRENHEIT_UPPER 300
+#define FAHRENHEIT_STEP 20
+
 void main() {
-    convertCelcius(100, 5);
-    convertFahrenheit(300, 20);
+    convertCelsius(CELSIUS_UPPER, CELSIUS_STEP);
+    convertFahrenheit(FAHRENHEIT_UPPER, FAHRENHEIT_STEP);
 }
