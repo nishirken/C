d166f31014b07fa40198611be8ee4b33d77227a0
Dmitrii Skurikhin <dskurikhin@devvela.com>
2017-09-18 23:37:31 +0500

    added chart creation function

--- src/stdWords.c
+++ src/stdWords.c
@@ -7,6 +7,7 @@
 #define SPACE 32
 #define BACK_SPACE 8
 #define BACK_SLASH 92
+#define BLACK_RECTANGLE_CODE 61
 
 int endOfFile(int value) {
     return value == EOF;
@@ -40,7 +41,74 @@ void newLineWords() {
     putchar('\n');
 }
 
+void countSymbols() {
+    int c = 0;
+    int numberCounts[10];
+    int tabsCount = 0;
+    int spacesCount = 0;
+
+    for (int i = 0; i < 10; ++i) {
+        numberCounts[i] = 0;
+    }
+
+    while (!endOfFile(c = getchar()) && c != NEW_LINE) {
+        if (c == SPACE) {
+            ++spacesCount;
+        } else if (c == TAB) {
+            ++tabsCount;
+        } else if (c >= '0' && c <= '9') {
+            ++numberCounts[c - '0'];
+        }
+    }
+    printf("numberCounts: ");
+    for (int i = 0; i < 10; ++i) {
+        printf("%d,", numberCounts[i]);
+    }
+    printf("\n");
+    printf("Spaces: %d\n", spacesCount);
+    printf("Tabs: %d\n", tabsCount);
+}
+
+void createSymbolCountChartBars() {
+    int c = 0;
+    int symbolCounts[300];
+    char line[300];
+
+    for (int i = 0; i < 300; ++i) {
+        symbolCounts[i] = 0;
+    }
+
+    int j;
+    for (j = 0; j < 300 - 1 && (!endOfFile(c = getchar())) && c != NEW_LINE; ++j) {
+        ++symbolCounts[c - '0'];
+        line[j] = c;
+    }
+    if (c == NEW_LINE) {
+        line[j] = c;
+        ++j;
+    }
+    line[j] = '\0';
+
+    int k = 0;
+    char prevSymbol;
+    while (line[k] != '\0' && line[k] != NEW_LINE) {
+        if (line[k] != prevSymbol) {
+            prevSymbol = line[k];
+            printf("Symbol: %c ", line[k]);
+
+            for (int i = 0; i < symbolCounts[line[k] - '0']; ++i) {
+                printf("%c", BLACK_RECTANGLE_CODE);
+            }
+            putchar(NEW_LINE);
+        }
+        ++k;
+    }
+    putchar(NEW_LINE);
+}
+
 void main() {
     /*countWords();*/
-    newLineWords();
+    /*newLineWords();*/
+    /*countSymbols();*/
+    createSymbolCountChartBars();
 }
