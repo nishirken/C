cfb774376504c61f7eaad659e20e6cee40520927
Dmitrii Skurikhin <dskurikhin@devvela.com>
2017-09-29 20:32:38 +0500

    added limit line function

---
+++ src/std/stdLimitLines.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include "utils.h"
+#include "chars.h"
+#include "../boolean.h"
+
+#define MAX_LINE_LENGTH 100
+
+// TODO: print in runtime
+void printLimitedLines() {
+    const int lineLimit = 10;
+    int i = 0;
+    int currentLineLength = 0;
+    char c = 0;
+
+    while ((c = getchar()) != ESC) {
+        currentLineLength++;
+
+        if (currentLineLength == lineLimit) {
+            putchar(NEW_LINE);
+            currentLineLength = 0;
+        }
+        putchar(c);
+
+        i++;
+    }
+}
+
+void main() {
+    printLimitedLines();
+}
