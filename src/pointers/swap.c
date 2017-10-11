cd253194771153aa473b5335674baf51499fcedc
Dmitrii Skurikhin <dskurikhin@devvela.com>
2017-10-11 23:08:57 +0500

    added swap function

---
+++ src/pointers/swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void main() {
+    int a = 1;
+    int b = 2;
+    printf("a: %d, b: %d\n", a, b);
+    swap(&a, &b);
+    printf("a: %d, b: %d\n", a, b);
+}
