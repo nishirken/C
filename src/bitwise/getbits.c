e135e4ddea8a97e47fc258781d0ff4293965581e
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2018-04-30 18:43:38 +0500

    add getbits reversed

--- src/bitwise/getbits.c
+++ src/bitwise/getbits.c
@@ -4,6 +4,13 @@ unsigned getbits(unsigned x, int p, int n) {
     return (x >> (p + 1 - n)) & ~(~0 << n);
 }
 
+unsigned getbitsReversed(unsigned x, int p, int n) {
+    return (x >> 32 - (p + n)) & ~(~0 << n);
+}
+
 void main() {
+    // printbits(32232);
     // printbits(getbits(32232, 15, 14));
+    // printbits(32211222);
+    // printbits(getbitsReversed(32211222, 9, 5));
 }
--- src/bitwise/setbits.c
+++
@@ -1,9 +0,0 @@
-#include "./utils.h"
-
-unsigned setbits(unsigned x, int p, int n, unsigned y) {
-    return x;
-}
-
-void main() {
-    printbits(setbits(7, 3, 2, 13)); // 0101 - 5
-}
